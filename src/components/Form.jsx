0989c11ffa68a92f6436ebefc662d034d08f394c
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-05 22:05:17 +0300

    Add form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
+import React from 'react';
 
-const Form = () => {
-  return (
-    <div>
+const Form = () => (
+  <div>
     <form onSubmit={handleSubmit}>
 
-        {field.map((input) => (
-          <div key={input.name}>
-            <label htmlFor={input.name}>{input.label}</label>
-            <input
-              type={input.type}
-              name={input.name}
-              id={input.name}
-              defaultValue={input.defaultValue || ''}
-              onChange={handleChange}
-              required={input.required}
-              accept="image/*"
-            />
-          </div>
-        ))}
-        <button type="submit" className="btn">{action}</button>
-      </form>
-    </div>
-  )
-}
+      {field.map((input) => (
+        <div key={input.name}>
+          <label htmlFor={input.name}>{input.label}</label>
+          <input
+            type={input.type}
+            name={input.name}
+            id={input.name}
+            defaultValue={input.defaultValue || ''}
+            onChange={handleChange}
+            required={input.required}
+            accept="image/*"
+          />
+        </div>
+      ))}
+      <button type="submit" className="btn">{action}</button>
+    </form>
+  </div>
+);
 
-export default Form
+export default Form;
