c0c5d85edbe68df2189cccfba094986d216e1483
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-05 23:19:22 +0300

    Add home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './stylesheets/app.scss';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import layout from './layouts';
+import Home from './components/Home';
 
 const App = () => {
   const HomeComponent = layout(Home)
   const router = createBrowserRouter([
-    { path: '/', action: () => <div>Home</div> },
+    { path: '/', element: <HomeComponent />},
   ]);
   return (
     <div className="app">
