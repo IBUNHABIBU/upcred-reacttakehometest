3d31e818e6acee877588d8ebc99da6240149fb63
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-05 23:10:42 +0300

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './stylesheets/app.scss';
-import { RouterProvider } from 'react-router-dom';
 
-const App = () => {(
-  <div className="app">
-    <RouterProvider router={router}>
-  </div>
-)};
+const App = () => {
+  const router = createBrowserRouter([
+    { path: '/', action: () => <div>Home</div> },
+  ]);
+  return (
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
 export default App;
