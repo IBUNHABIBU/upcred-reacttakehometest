1ddb98bb8172260b86ce286e484c9c02fbc1314c
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-05 22:14:13 +0300

    Add proptypes

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,33 +1,39 @@
 import React, { useState } from 'react';
 
-const Form = ({field, onSubmit, action}) => {
-    const [formDate, setFormDate] = useState({});
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevState) => ({ ...prevState, [name]: value }));
-    };
-    return(
-  <div>
-    <form onSubmit={handleSubmit}>
+const Form = ({ field, onSubmit, action }) => {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
 
-      {field.map((input) => (
-        <div key={input.name}>
-          <label htmlFor={input.name}>{input.label}</label>
-          <input
-            type={input.type}
-            name={input.name}
-            id={input.name}
-            defaultValue={input.defaultValue || ''}
-            onChange={handleChange}
-            required={input.required}
-            accept="image/*"
-          />
-        </div>
-      ))}
-      <button type="submit" className="btn">{action}</button>
-    </form>
-  </div>
-)
+        {field.map((input) => (
+          <div key={input.name}>
+            <label htmlFor={input.name}>{input.label}</label>
+            <input
+              type={input.type}
+              name={input.name}
+              id={input.name}
+              defaultValue={input.defaultValue || ''}
+              onChange={handleChange}
+              required={input.required}
+              accept="image/*"
+            />
+          </div>
+        ))}
+        <button type="submit" className="btn">{action}</button>
+      </form>
+    </div>
+  );
+};
+
+Form.propTypes = {
+    field: PropTypes.arrayOf(PropTypes.object).isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    action: PropTypes.string.isRequired,
 };
 
 export default Form;
