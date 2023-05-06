738b83ce2fb8ab43646bb1774338bbcc974f2cc3
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-06 15:50:02 +0300

    Add Google login

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import FacebookComponent from './FacebookComponent'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <FacebookComponent />
+    </div>
   )
 }
 
