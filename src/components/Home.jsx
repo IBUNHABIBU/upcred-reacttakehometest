b9893fcd7197ef58affd6a9057c8368640323bff
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-06 09:21:27 +0300

    Make the home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ReactFacebookLogin from 'react-facebook-login';
+import FacebookLogin from 'react-facebook-login';
 
 const Home = () => {
   const [user, setUser] = useState({
@@ -12,24 +12,26 @@ const Home = () => {
 
   const responseFacebook = (response) => {
     console.log(response);
-  }
+  };
 
   const componentClicked = () => {
     console.log('clicked');
-  }
+  };
 
   let fbContent;
-  if (user.isLoggedIn) { 
+  if (user.isLoggedIn) {
     fbContent = null;
   } else {
-    fbContent = ( <FacebookLogin
-      appId="216650907742077"
-      autoLoad={true}
-      fields="name,email,picture"
-      onClick={componentClicked}
-      callback={responseFacebook} />);
+    fbContent = (
+      <FacebookLogin
+        appId="216650907742077"
+        autoLoad
+        fields="name,email,picture"
+        onClick={componentClicked}
+        callback={responseFacebook}
+      />
+    );
   }
-   
 
   return (
     <div>{fbContent}</div>
