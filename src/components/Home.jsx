27c8842e3e861d029b5f11c279af6f66e19635f1
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-06 18:05:04 +0300

    Add googlelogout component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { gapi } from 'gapi-script';
 import FacebookComponent from './FacebookComponent';
 import GoogleComponent from './GoogleComponent';
+import GoogleLogoutComponent from './GoogleLogoutComponent';
 
-const Home = () => (
+const Home = () => {
+  return(
   <div>
     <FacebookComponent />
     <GoogleComponent />
+    <GoogleLogoutComponent />
   </div>
-);
+)};
 
 export default Home;
