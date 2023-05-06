5ee14260b5e35e41e2b6a6674e8b87253810a305
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-06 15:49:14 +0300

    Add Google login

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,49 +1,9 @@
-import { useState } from 'react';
-import FacebookLogin from 'react-facebook-login';
+import React from 'react'
 
 const Home = () => {
-  const [user, setUser] = useState({
-    id: '',
-    isLoggedIn: false,
-    name: '',
-    email: '',
-
-  });
-
-  const navigate = useNavigate();
-
-  const responseFacebook = (response) => {
-    console.log(response);
-    setUser({
-      id: response.id,
-      isLoggedIn: true,
-      name: response.name,
-      email: response.email,
-    });
-  };
-
-  const componentClicked = () => {
-    console.log('clicked');
-  };
-
-  let fbContent;
-  if (user.isLoggedIn) {
-    fbContent = null;
-  } else {
-    fbContent = (
-      <FacebookLogin
-        appId="216650907742077"
-        autoLoad
-        fields="name,email,picture"
-        onClick={componentClicked}
-        callback={responseFacebook}
-      />
-    );
-  }
-
   return (
-    <div>{fbContent}</div>
-  );
-};
+    <div>Home</div>
+  )
+}
 
-export default Home;
+export default Home
