dab681be236a80ffab488aa045ce0a9af453e4c1
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-06 16:07:00 +0300

    Add Google login

--- src/components/GoogleComponent.jsx
+++ src/components/GoogleComponent.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 
-const GoogleComponent = () => (
-  <div>
+const GoogleComponent = () => {
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+  return (
+    <div>
       <GoogleLogin
-    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-    buttonText="Login"
-    onSuccess={responseGoogle}
-    onFailure={responseGoogle}
-    cookiePolicy={'single_host_origin'}
-  />
-  </div>
-);
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy="single_host_origin"
+      />
+    </div>
+  );
+};
 
 export default GoogleComponent;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import FacebookComponent from './FacebookComponent';
+import GoogleComponent from './GoogleComponent';
 
 const Home = () => (
   <div>
     <FacebookComponent />
+    <GoogleComponent />
   </div>
 );
 
