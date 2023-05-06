9770b1c47a5b75c69cb366a28ea60c690d3049b5
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-06 16:02:29 +0300

    Add constants

--- src/components/GoogleComponent.jsx
+++ src/components/GoogleComponent.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const GoogleComponent = () => {
-  return (
-    <div>GoogleComponent</div>
-  )
-}
+const GoogleComponent = () => (
+  <div>
+      <GoogleLogin
+    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
+  </div>
+);
 
-export default GoogleComponent
+export default GoogleComponent;
