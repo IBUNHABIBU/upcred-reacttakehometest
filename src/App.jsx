416543d675a9b9822b3d5cc064708e9b5a36557a
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-07 17:54:59 +0300

    Add products component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import layout from './layouts';
 import Home from './components/Home';
 import Login from './components/Login';
+import Products from './components/Products';
 
 const App = () => {
   const HomeComponent = layout(Home);
   const LoginComponent = layout(Login);
+  const ProductComponent = layout(Products);
+
   const router = createBrowserRouter([
     { path: '/', element: <HomeComponent /> },
     { path: '/login', element: <LoginComponent /> },
+    { path: '/products', element: <ProductComponent /> },
   ]);
   return (
     <div className="app">
