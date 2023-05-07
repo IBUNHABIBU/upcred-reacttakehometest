b69a420d04f15e7e83c61565080170f1f85f3f20
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-07 17:00:18 +0300

    Style the navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => (
-  <div>
-    <ul>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul>
-  </div>
+  <nav className="nav">
+      <div className="nav__logo">
+        <span />
+        <NavLink to="/" className="link">Salum Habibu</NavLink>
+      </div>
+      <div className="nav__lists">
+        {
+        filteredList.map((list) => (
+          <NavLink
+            to={list.path}
+            key={list.id}
+            className={({ isActive }) => (isActive ? 'link link--active' : 'link')}
+          >
+            {list.name}
+          </NavLink>
+        ))
+      }
+        <button type="submit" className="btn" onClick={handleLogout}>Logout</button>
+      </div>
+    </nav>
 );
 
 export default Nav;
