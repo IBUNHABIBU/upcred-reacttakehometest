4b89ffb1820933f70b0971fba3f8caa0c7a9a12a
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-07 22:50:20 +0300

    Make user states

--- src/components/FacebookComponent.jsx
+++ src/components/FacebookComponent.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import {FacebookLogin, FacebookLogout } from 'react-facebook-login';
+import { FacebookLogin, FacebookLogout } from 'react-facebook-login';
 import { useDispatch, useSelector } from 'react-redux';
 import { facebookAppId } from '../constants/constants';
-import { loginFacebook } from '../redux/actions/actions';
+import { loginFacebook, logout } from '../redux/actions/actions';
 
 const FacebookComponent = () => {
   const user = useSelector((state) => state.user);
@@ -17,8 +17,8 @@ const FacebookComponent = () => {
     navigate('/products');
   };
 
-  const componentClicked = () => {
-    console.log('clicked');
+  const handleLogout = () => {
+    dispatch(logout());
   };
 
   let fbContent;
@@ -26,17 +26,18 @@ const FacebookComponent = () => {
     fbContent = null;
   } else {
     fbContent = (
-      <div><FacebookLogin
-        appId={facebookAppId}
-        autoLoad
-        fields="name,email,picture"
-        onClick={componentClicked}
-        callback={responseFacebook}
-      />
-      <FacebookLogout
-  appId={facebookAppId}
-  onLogout={handleLogout}
-/>
+      <div>
+        <FacebookLogin
+          appId={facebookAppId}
+          autoLoad
+          fields="name,email,picture"
+          onClick={componentClicked}
+          callback={responseFacebook}
+        />
+        <FacebookLogout
+          appId={facebookAppId}
+          onLogout={handleLogout}
+        />
       </div>
     );
   }
