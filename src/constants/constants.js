0508d8724763541fbe540435b3eba2d0155bd3f3
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-07 17:02:10 +0300

    Style the navbar

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,21 @@
 export const cliendId = '456726655991-c39d25o9lfmvfbog92pparscqvjmr3tk.apps.googleusercontent.com';
 export const clientSecret = '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';
 export const facebookAppId = '216650907742077';
+export const list = [
+    {
+      id: 1,
+      name: 'Home',
+      path: '/',
+    },
+    {
+      id: 2,
+      name: 'Products',
+      path: '/products',
+    },
+   
+    {
+      id: 5,
+      name: 'Admin Panel',
+      path: '/admin',
+    },
+  ];
