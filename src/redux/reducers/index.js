b1ede4c4030119ec6e8df5f3c13634d9f9aa8e05
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-07 18:49:09 +0300

    Add combine reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import userReducer from './userReducer';
+import productsReducer from './productsReducer';
 
 const reducers = combineReducers({
-    user: userReducer,
-    products: productsReducer,
-})
+  user: userReducer,
+  products: productsReducer,
+});
 
 export default reducers;
