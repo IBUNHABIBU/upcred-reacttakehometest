3c9ee8151aa69fffbd43ed15d35c97ef5176f39d
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-07 17:28:17 +0300

    Style the navbar

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,10 +15,9 @@ const Login = () => {
   }, []);
 
   return (
-    <div>
+    <div className="login">
       <FacebookComponent />
       <GoogleComponent />
-      <GoogleLogoutComponent />
     </div>
   );
 };
