6f30e23068448f2cf76cc399f16fb1bc6d62ce06
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-07 23:32:31 +0300

    Make user states

--- src/components/GoogleComponent.jsx
+++ src/components/GoogleComponent.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import GoogleLogin from 'react-google-login';
 import { cliendId } from '../constants/constants';
+import { useDispatch } from 'react-redux';
+import { loginGoogle } from '../redux/actions/actions';
 
 
 const GoogleComponent = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const responseGoogle = (response) => {
-    
+    dispatch(loginGoogle(response);
     navigate('/products');
   };
 
