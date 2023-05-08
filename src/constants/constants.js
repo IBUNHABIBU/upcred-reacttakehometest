f02cf44ad01e2ce55898fc960fca1de299ea8320
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-08 17:30:43 +0300

    Add gushup api keys

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,8 @@
 export const cliendId = '456726655991-c39d25o9lfmvfbog92pparscqvjmr3tk.apps.googleusercontent.com';
 export const clientSecret = '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';
 export const facebookAppId = '216650907742077';
+export const gushupAppId = '4000206800';
+export const gushupApiKey = 'e8b66wlv57x7n8juxhyfqqfvdg1rtity';
 export const list = [
   {
     id: 1,
