97dd96ba147b734f3ba508a86d8e1a24262bb139
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-08 17:05:27 +0300

    Add otp component

--- src/components/OtpComponent.jsx
+++ src/components/OtpComponent.jsx
@@ -7,6 +7,20 @@ const OtpComponent = () => {
 
   const generateOtp = () => {
     const otpValue = Math.floor(832 + Math.random() * 9000).toString();
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+             method: 'sendMessage',
+             sendTo: phoneNo,
+             msg: `Your OTP is ${otpValue}`,
+                msgType: 'TEXT',
+                userId: '2000044622',
+                password: '123456',
+                auth_scheme: 'plain',
+             })
+
   };
   return (
     <div>OtpComponent</div>
