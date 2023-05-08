f9456b5beb711cd502695eb38569e75d7cb90ead
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-08 17:01:55 +0300

    Add otp component

--- src/components/OtpComponent.jsx
+++ src/components/OtpComponent.jsx
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 
 const OtpComponent = () => {
   const [phoneNo, setPhoneNo] = useState('');
+  const [otp, setOtp] = useState('');
+  const [sentOtp, setSentOtp] = useState('');
+
+  const generateOtp = () => {
+    const otpValue = Math.floor(832 + Math.random() * 9000).toString();
+  }
   return (
     <div>OtpComponent</div>
   );
