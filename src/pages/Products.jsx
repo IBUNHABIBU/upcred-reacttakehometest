1eb08517e6dbda6276a6e57d0664f99b255ad4a3
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-09 22:54:40 +0300

    Add a cart

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -23,21 +23,24 @@ const Products = () => {
   };
   return (
     <div className="checkout">
-    <div className="products">
-      {products.map((product) => (
-        <div className="product" key={product.id}>
-          <img src={product.image} alt={product.title} className="product__image" />
-          <h3 className="product__title">{product.title.slice(0, 30)}</h3>
-          <p className="product__description">{product.description.slice(0, 120).concat('...')}</p>
-          <p className="product__price">{product.price}</p>
-          <button type="submit" className="btn" onClick={() => AddCart(product.id)}>
-            AddToCart (
-            {cart.quantity}
-            )
-          </button>
-        </div>
-      ))}
-    </div>
+      <div className="checkout__header">
+        <button type='submit'>Checkout {carts.quantity}</button>
+      </div>
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.id}>
+            <img src={product.image} alt={product.title} className="product__image" />
+            <h3 className="product__title">{product.title.slice(0, 30)}</h3>
+            <p className="product__description">{product.description.slice(0, 120).concat('...')}</p>
+            <p className="product__price">{product.price}</p>
+            <button type="submit" className="btn" onClick={() => AddCart(product.id)}>
+              AddToCart (
+              {cart.quantity}
+              )
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
