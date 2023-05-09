7fcc9f0c927e26f1ab64dc905962d317ff72b38b
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-10 00:03:38 +0300

    Add Carts page

--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Carts = () => {
   const cart = useSelector((state) => state.cart);
-  return(
-  <div>
-    <h1>Carts</h1>
-    {
+  return (
+    <div>
+      <h1>Carts</h1>
+      {
       cart.map((item) => (
         <div key={item.id}>
           <h3>{item.title}</h3>
           <p>{item.quantity}</p>
         </div>
+      ))
     }
-  </div>
-)};
+    </div>
+  );
+};
 
 export default Carts;
