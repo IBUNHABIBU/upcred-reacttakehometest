ce56acd151fd8280f700e885c940c953987efe89
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-09 23:51:38 +0300

    Add Carts page

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import productsReducer from './productsReducer';
+import cartsReducer from './cartsReducer';
 
 const reducers = combineReducers({
   user: userReducer,
   products: productsReducer,
+  carts: cartsReducer,
 });
 
 export default reducers;
