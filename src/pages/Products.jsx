a376da213a661405a921d7b209fd108c18df53c5
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-09 19:16:54 +0300

    Add products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
-
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      dispatch(setProducts(data));
+    };
+    fetchProducts();
+  }, []);
   return (
     <div>Products</div>
   );
