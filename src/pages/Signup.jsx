3eec283ac389fd8e279c8b8ba5d64773d014ef4d
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-09 17:38:42 +0300

    Add a signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,36 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-    const userLogin = useSelector((state) => state.register);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const [errors, setErrors] = useState('');
-    const [message, setMessage] = useState('');
-    const handleSubmit = (data) => {
-      axios.post(`${urlBase}/users`, {
-        name: data.name,
-        email: data.email,
-        password: data.password,
-        password_confirmation: data.password_confirmation,
-      },
-      { withCredentials: true }).then((response) => {
-        if (response.data.status === 'created') {
-          dispatch(setUser(response.data));
-          navigate('/models');
-          setMessage('User created successfully');
-        }
-        setErrors(response.data.error);
-      });
-    };
-  
-    return (
-      <div className="signup">
-        <div className="signup__header">
-          <h1>Register</h1>
-        </div>
-        <Form
-          errors={errors}
-          field={
+  const userLogin = useSelector((state) => state.register);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState('');
+  const [message, setMessage] = useState('');
+  const handleSubmit = (data) => {
+    axios.post(`${urlBase}/users`, {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+      password_confirmation: data.password_confirmation,
+    },
+    { withCredentials: true }).then((response) => {
+      if (response.data.status === 'created') {
+        dispatch(setUser(response.data));
+        navigate('/models');
+        setMessage('User created successfully');
+      }
+      setErrors(response.data.error);
+    });
+  };
+
+  return (
+    <div className="signup">
+      <div className="signup__header">
+        <h1>Register</h1>
+      </div>
+      <Form
+        errors={errors}
+        field={
           [
             {
               name: 'name', type: 'name', label: 'name', required: true,
@@ -46,15 +47,15 @@ const Signup = () => {
             },
           ]
         }
-          onSubmit={(formData) => handleSubmit(formData)}
-          action="Signup"
-        />
-        <p>
-          Already Member?
-          <Link to="/login" className="link"> Login</Link>
-        </p>
-      </div>
-    );
-}
+        onSubmit={(formData) => handleSubmit(formData)}
+        action="Signup"
+      />
+      <p>
+        Already Member?
+        <Link to="/login" className="link"> Login</Link>
+      </p>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
