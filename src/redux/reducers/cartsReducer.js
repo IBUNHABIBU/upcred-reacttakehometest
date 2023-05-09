6c4a2fcb38ae868d20c5cec1d6dec667f829d6a9
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-09 23:50:56 +0300

    Add Carts page

---
+++ src/redux/reducers/cartsReducer.js
@@ -0,0 +1,14 @@
+import actionTypes from "../constants";
+
+const initialState = [];
+
+const cartsReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case actionTypes.ADD_TO_CART:
+      return [...payload];
+    default:
+      return state;
+  }
+};
+
+export default cartsReducer;
