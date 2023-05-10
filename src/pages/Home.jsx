8a6b6d6677df91281e429ed0b351475c961f9edb
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-10 22:10:08 +0300

    Fix the errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Navigate } from 'react-router-dom';
 import Signup from './Signup';
 
 const Home = () => {
+  const isLogged = localStorage.getItem('isLogged');
   if (isLogged) {
     return (
       <>
