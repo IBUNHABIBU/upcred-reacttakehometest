937527efa3b793a0f57313c466917e2a4b44e5b1
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-10 22:42:36 +0300

    Fix the errors

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,6 @@
 export const cliendId = '456726655991-c39d25o9lfmvfbog92pparscqvjmr3tk.apps.googleusercontent.com';
 export const clientSecret = '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';
 export const facebookAppId = '216650907742077';
-export const gushupAppId = '4000206800';
-export const gushupApiKey = 'e8b66wlv57x7n8juxhyfqqfvdg1rtity';
 export const twillioBaseUrl = 'https://api.twilio.com/2010-04-01';
 export const fakeStoreUrl = 'https://fakestoreapi.com';
 export const urlBase = 'https://car.darlive.cyou';
