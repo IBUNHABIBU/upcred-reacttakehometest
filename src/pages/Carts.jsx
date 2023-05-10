539b3dd5abc2bc5b2f76de1d5faa15f57f60fe5d
IBUNHABIBU <ibunhabib@gmail.com>
2023-05-10 07:17:35 +0300

    Add all carts

--- src/pages/Carts.jsx
+++ src/pages/Carts.jsx
@@ -5,19 +5,12 @@ import { fakeStoreUrl } from '../constants';
 
 const Carts = () => {
   const cart = useSelector((state) => state.carts.products);
-  console.log(cart);
+  const products = useSelector((state) => state.products);
+  console.log(products);
   
   return (
     <div>
-      <h1>Carts</h1>
-      {
-      cart && cart.length > 0 && cart.map((item) => (
-        <div key={item.id}>
-          <h3>{item.title}</h3>
-          <p>{item.quantity}</p>
-        </div>
-      ))
-    }
+   hello
     </div>
   );
 };
